2b445a4afbe3221d3e0b611cdbe2560c9c0dffae
Stephen Koppel <stephen.koppel@gmail.com>
2019-02-01 20:26:54 -0500

    lua tweak

--- stats/cat_primary.lua
+++ stats/cat_primary.lua
@@ -6,7 +6,7 @@ local catInit = init
 function init()
 	catInit()
     if world.entitySpecies(entity.id()) == "cat" then
-		status.setStatusProperty("mouthPosition", {0, -1})
+		status.setStatusProperty("mouthPosition", {0, -1.2})
        	status.addPersistentEffects("CatPersistantEffects",{{stat = "fallDamageMultiplier", effectiveMultiplier = 0.70}})
     end
 end
